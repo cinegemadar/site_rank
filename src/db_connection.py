d03212fd31c6aecfb67b17f9a82ab55582f3e920
Balogh David <balogh.david@cae.com>
2016-11-16 15:43:15 +0100

    add autoincrement for containment

--- src/db_connection.py
+++ src/db_connection.py
@@ -1,10 +1,13 @@
-import csv, hashlib, json
+import csv
+import hashlib
+import json
 from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from site_rank_global import logger
+from site_rank_global import logger, db_location
 
 Base = declarative_base()
+
 
 class Site(Base):
     '''
@@ -12,8 +15,8 @@ class Site(Base):
     '''
     __tablename__ = 'sites'
 
-    id = Column(Integer,primary_key=True, autoincrement=True)
-    url = Column(String(40)) 
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    url = Column(String(40))
     predicted_category = Column(String(20))
     category = Column(String(20))
 
@@ -23,13 +26,14 @@ class Site(Base):
     def __str__(self):
         return self.__repr__()
 
+
 class Word(Base):
     '''
     ORM class to representing site entries.
     '''
     __tablename__ = 'words'
 
-    id = Column(Integer,primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     word = Column(String(30))
 
     def __repr__(self):
@@ -38,7 +42,6 @@ class Word(Base):
     def __str__(self):
         return self.__repr__()
 
-        
 
 class Containment(Base):
     '''
@@ -46,57 +49,75 @@ class Containment(Base):
     '''
     __tablename__ = 'containment'
 
-    id = Column(Integer,primary_key=True, autoincrement=True)
+    id = Column(Integer, primary_key=True, autoincrement=True)
     site_id = Column(Integer, ForeignKey('sites.id'), autoincrement=True)
     word_id = Column(Integer, ForeignKey('words.id'), autoincrement=True)
-    
+
     count = Column(Integer)
 
     def __repr__(self):
-        return '[Word no:] {}\n[Site no:] {}\n[Count:] {}\n==============='.format(self.word_id, self.site_id, self.count)
+        return '\n[Word no:] {}\n[Site no:] {}\n[Count:] {}\n===============\n'.format(self.word_id, self.site_id, self.count)
 
     def __str__(self):
         return self.__repr__()
 
-class RankDBHandler():
 
+class RankDBHandler():
+    '''
+    DB connection handler.
+    '''
 
     def __init__(self):
-        
-        self.engine = create_engine('sqlite:///..\\db\\ranking.db', echo = False)
 
+        #Create DB engine.
+        self.engine = create_engine(db_location, echo=False)
+
+        #Generate metadata.
         Base.metadata.create_all(self.engine, checkfirst=True)
 
-        self.Session = sessionmaker(bind=self.engine)
-        self.current_session = self.Session()
+        #Create session.
+        Session = sessionmaker(bind=self.engine)
+        self.current_session = Session()
 
     def __exit__(self):
+        '''
+        Make sure to commit changes and close connection uppon destruction.
+        '''
+
         self.current_session.commit()
         self.current_session.close()
 
-    def commit(self):
-        '''
-        Wrapper to safe commit.
-        '''
-        try:
-            self.current_session.commit()
-        except:
-            self.current_session.rollback()
-            raise
 
-    def test(self):
+    def test_connection(self):
+        '''
+        Returns all site.
+        '''
         return [x for x in self.current_session.query(Site)]
 
-    def update_containment(self, site, word, count):
+    def update_containment(self, site, word, count=1, increment=True):
+        '''
+        Insert or update cointainment data.
+        '''
+
+        #Check in DB if entry exists.
         result = self.current_session.query(Containment).filter(Containment.site_id == site, Containment.word_id == word)
-        
+
+        #If exits, update counter.
         if(result.count() > 0):
             logger.info('Update entry.')
-            result[0].count = count
-            logger.info(result[0])
+
+            if increment:
+                result[0].count += 1
+            else:
+                result[0].count = count
+            
+            logger.debug(result[0])
+
+        #Otherwise create new entry
         else:
             logger.info('Add new entry.')
-            self.current_session.merge(Containment(site_id=site,word_id=word,count=count))
+            self.current_session.merge(Containment(
+                site_id=site, word_id=word, count=count))
         self.current_session.commit()
 
 ''' TEST
@@ -109,10 +130,6 @@ for i in t:
     print(i)
 '''
 t_handler = RankDBHandler()
-t_handler.update_containment(5,5,5)
-t_handler.update_containment(5,10,5)
-t_handler.update_containment(5,15,5)
-t_handler.update_containment(5,20,5)
-t_handler.update_containment(5,25,50)
+t_handler.update_containment(5, 6)
 
-#print(t_handler.test())
+# print(t_handler.test())
