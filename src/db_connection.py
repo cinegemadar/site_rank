c0b5a75ac10d5348ca629c8547c003f5304497f2
Balogh David <balogh.david@cae.com>
2016-11-16 15:20:27 +0100

    update

--- src/db_connection.py
+++ src/db_connection.py
@@ -2,6 +2,7 @@ import csv, hashlib, json
 from sqlalchemy import Column, Integer, String, create_engine, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from site_rank_global import logger
 
 Base = declarative_base()
 
@@ -43,7 +44,7 @@ class Containment(Base):
     '''
     ORM class to representing site entries.
     '''
-    __tablename__ = 'contaiment'
+    __tablename__ = 'containment'
 
     id = Column(Integer,primary_key=True, autoincrement=True)
     site_id = Column(Integer, ForeignKey('sites.id'), autoincrement=True)
@@ -57,6 +58,47 @@ class Containment(Base):
     def __str__(self):
         return self.__repr__()
 
+class RankDBHandler():
+
+
+    def __init__(self):
+        
+        self.engine = create_engine('sqlite:///..\\db\\ranking.db', echo = False)
+
+        Base.metadata.create_all(self.engine, checkfirst=True)
+
+        self.Session = sessionmaker(bind=self.engine)
+        self.current_session = self.Session()
+
+    def __exit__(self):
+        self.current_session.commit()
+        self.current_session.close()
+
+    def commit(self):
+        '''
+        Wrapper to safe commit.
+        '''
+        try:
+            self.current_session.commit()
+        except:
+            self.current_session.rollback()
+            raise
+
+    def test(self):
+        return [x for x in self.current_session.query(Site)]
+
+    def update_containment(self, site, word, count):
+        result = self.current_session.query(Containment).filter(Containment.site_id == site, Containment.word_id == word)
+        
+        if(result.count() > 0):
+            logger.info('Update entry.')
+            result[0].count = count
+            logger.info(result[0])
+        else:
+            logger.info('Add new entry.')
+            self.current_session.merge(Containment(site_id=site,word_id=word,count=count))
+        self.current_session.commit()
+
 ''' TEST
 engine = create_engine('sqlite:///..\\db\\ranking.db', echo = False)
 Base.metadata.create_all(engine, checkfirst=True)
@@ -65,3 +107,12 @@ session = Session()
 t = session.query(Site)
 for i in t:
     print(i)
+'''
+t_handler = RankDBHandler()
+t_handler.update_containment(5,5,5)
+t_handler.update_containment(5,10,5)
+t_handler.update_containment(5,15,5)
+t_handler.update_containment(5,20,5)
+t_handler.update_containment(5,25,50)
+
+#print(t_handler.test())
--- src/site_crawler.py
+++ src/site_crawler.py
@@ -1,10 +1,58 @@
 from lxml import html
+from site_rank_global import logger
 import requests
 
-page = requests.get('http://Kidshealth.org/en/teens/')
+class SiteCrawler():
+    '''
+    Simple website text crawler.
+    '''
 
-tree = html.fromstring(page.content)
+    def crawl(self, site, lang=''):
 
-content = tree.xpath('//b/text()')
+        self.site = site
 
-print('\n'.join(content))
+        try:
+            self.load_site()
+        except requests.exceptions.MissingSchema:
+            logger.warning('Missing schema')
+            self.site = 'http://' + self.site 
+            self.load_site()
+        except requests.exceptions.ConnectionError:
+            logger.error('Cannot open {}'.format(self.site))
+            return ''
+
+        try:
+            self.build_structure()
+        except UnicodeDecodeError:
+            logger.error('{} site is Unicode encoded. We currently support UTF-8 Only. Skipping this site for now.'.format(self.site))
+            return ''
+
+        return ' '.join(self.site_text)
+
+    def load_site(self):
+        
+        logger.info('Loading {}'.format(self.site))
+        self.page = requests.get(self.site)
+        
+
+
+    def build_structure(self):
+        
+        
+        
+        #Build HTML structure.
+        self.tree = html.fromstring(self.page.content.decode('utf-8'))
+        #Parse text data from site.
+        self.site_text = self.tree.xpath('//body//*[not(self::script)]/text()')
+        
+        
+        
+'''TEST
+from nltk_wrapper import NltkWrapper
+
+crawler = SiteCrawler()
+nltk = NltkWrapper()
+words = crawler.crawl('google.com')
+s_words = nltk.get_sanitized_word_list(words)
+print(s_words)
+'''
